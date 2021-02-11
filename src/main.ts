5f8c1d9f727f57eb0fa37235d40558913acc80e6
Semyon Levin <levin.semen@gmail.com>
2021-02-11 22:09:12 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
 import * as core from '@actions/core'
+import {context} from '@actions/github'
+import simpleGit from 'simple-git'
+import {SimpleGit} from 'simple-git/promise'
+import workspacePath from './internal/workspacePath'
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 async function run(): Promise<void> {
     try {
-        core.info("action logic")
+        const githubToken = core.getInput('githubToken', {required: true})
+        core.setSecret(githubToken)
+
+        const message = core.getInput('message', {required: true})
+
+        const files = core.getInput('files').split('\n')
+            .map(line => line.trim())
+            .filter(line => line.length > 0)
+
+
+        const git = simpleGit(workspacePath)
+        const status = await git.status(files)
+        if (status.files.length === 0) {
+            core.info("No files were changed, nothing to commit")
+            return
+        }
+
+        const user = await getGitConfig(git, 'user.name1') || context.actor || context.repo.owner
+        await git.addConfig('user.name', user)
+        await git.addConfig('user.email', `${user}@users.noreply.github.com`)
+
+        await git.commit(message, files)
 
     } catch (error) {
         core.setFailed(error)
@@ -13,3 +38,10 @@ async function run(): Promise<void> {
 
 //noinspection JSIgnoredPromiseFromCall
 run()
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+async function getGitConfig(git: SimpleGit, configKey: string): Promise<string | undefined> {
+    return git.raw('config', '--get', configKey)
+        .then(text => text.trim())
+}
